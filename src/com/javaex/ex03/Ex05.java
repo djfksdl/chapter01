735b2f163dee7cf602a8056eca4d38ae39a0c4e3
djfksdl <djfksdl@gmail.com>
2023-12-26 17:58:25 +0900

    예제문제 풀기

--- src/com/javaex/ex03/Ex05.java
+++ src/com/javaex/ex03/Ex05.java
@@ -2,7 +2,8 @@ package com.javaex.ex03;
 
 import java.util.Scanner;
 
-public class Ex04 {
+public class Ex05 {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -10,12 +11,13 @@ public class Ex04 {
 		int time = sc.nextInt();
 		
 		if(time <= 8) {
-			System.out.println("임금은 " + time*10000 +"원 입니다.");
-			
+			System.out.println("임금은 " + time*10000 + "원 입니다.");
 		} else {
-			System.out.println("임금은 " + (80000+ (time -8)*12000) + "원 입니다.");
+			System.out.println("임금은 " + (80000 + (time-8)*15000) + "원 입니다.");
 		}
 		
 		sc.close();
+
 	}
+
 }
